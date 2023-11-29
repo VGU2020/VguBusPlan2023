27899ff216188e455482f1f352693fdaaa8296d8
NOMAD <=>
2023-11-29 09:18:32 +0700

    congesstion_level_update

--- app/app/controllers.py
+++
@@ -1,8 +0,0 @@
-from .services import *
-
-def renderMap():
-    network = getData()
-    map = map_realtime(network=network)
-    
-    return mapTag(map=map)
-
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from app import create_app
+from webapp import create_app
 
 app = create_app()
 
